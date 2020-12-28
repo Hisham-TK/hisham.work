fb7bb04f99f163983144f54001fcd8332c1acfab
Hisham T. Kamal-Al-Din <hisham.taha.work@gmail.com>
2020-12-28 10:20:53 +0000

    edits

---
+++ src/resumeData.js
@@ -0,0 +1,372 @@
+export default {
+  main: {
+    name: "Hisham Taha Kamal-Al-Din",
+    occupation: "Software Developer (MEAN Stack)",
+    description:
+      "Creative, detail-oriented, ambitious, assiduous, animated JavaScript developer",
+    image: "profilepic.jpg",
+
+    bio: `Node/TypeScript/JavaScript Developer, GIS Analyst.
+Graduate BA, Department of Geography and GIS, trainee in the (GIS, and MEAN Stack) Tracks at ITI Diploma. finish scholarship of the Institute of Technological Innovation in Egypt and many other courses.`,
+    gisWebsite: "https://hisham-taha.portfoliobox.net/projects",
+    contactmessage:
+      "If I can be of assistance, please do not hesitate to contact me...",
+    email: "hisham.taha.work@gmail.com",
+    phone: "+20122-346-7708",
+    address: {
+      street: "Idrsie school, st",
+      city: "Alexandria, Egypt",
+      state: "Hadid we solp",
+      zip: "21575",
+    },
+    website: "http://www.hisham.work",
+
+    resumedownload:
+      "https://1drv.ms/b/s!AuHMK3CYszsBhN8fq6WfR_AIkYDf6g?e=HXxCVp",
+    social: [
+      {
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/hisham-taha-kamal-al-din/",
+        className: "fa fa-linkedin",
+      },
+      {
+        name: "github",
+        url: "https://github.com/Hisham-TK?tab=repositories",
+        className: "fa fa-github",
+      },
+      {
+        name: "facebook",
+        url: "https://www.facebook.com/Hisham.Taha.Kamal.AlDin",
+        className: "fa fa-facebook",
+      },
+      //   {
+      //     name: "twitter",
+      //     url: "http://twitter.com",
+      //     className: "fa fa-twitter",
+      //   },
+      //   {
+      //     name: "google-plus",
+      //     url: "http://googleplus.com/tbakerx",
+      //     className: "fa fa-google-plus",
+      //   },
+      //   {
+      //     name: "instagram",
+      //     url: "http://instagram.com/tbaker_x",
+      //     className: "fa fa-instagram",
+      //   },
+      //   {
+      //     name: "skype",
+      //     url: "http://skype.com",
+      //     className: "fa fa-skype",
+      //   },
+    ],
+  },
+  resume: {
+    work: [
+      {
+        company: "Index Group",
+        title: "Intermediate -> Senior Node.js",
+        years: "Jan 2020 - Present",
+        description:
+          "Fix problems and add new features to any Applications (Courier, Events, Home, Food, Sharing)",
+      },
+      {
+        company: "MicroTec SA",
+        title: "Junior Software Developer (MEAN Stack)",
+        years: "Apr 2020 - Des 2020",
+        description:
+          "Build an ERP (Enterprise resource planning) System from business (JIRA User Stories, Wireframes) in Agile process until secured APIs.",
+      },
+    ],
+
+    education: [
+      {
+        school: "Information Technology Institute",
+        degree: "IT Diploma at MEAN Stack Development",
+        graduated: "Mar 2019",
+        description: `Track courses :
+1. Object-oriented using C#
+2. Client-side technologies (HTML 4-5, CSS 2-3, JavaScript 5-6 Design patterns)
+3. jQuery + jQuery UI
+4. Database (MS SQL Server, MongoDB)
+5. Node, Express, Ajax
+6. Angular 7
+7. Responsive web (Bootstrap 4)
+8. Freelancing, Communication, Presentation, CV writing skills`,
+      },
+      {
+        school: "Trainee as GIS Specialist",
+        degree: "Next Technology Leaders (NTL)",
+        graduated: "Jul 2018",
+        description: `Specialization Courses :
+1. Fundamentals of GIS
+2. GIS Data Formats, Design, and Quality
+3. Geospatial and Environmental Analysis
+4. Imagery, Automation, and Applications
+5. Geospatial Analysis Project`,
+      },
+      {
+        school: "Information Technology Institute",
+        degree: "IT Diploma at MEAN Stack Development",
+        graduated: "Apr 2018",
+        description: `Track courses :
+1. GIS Fundamentals
+2. ArcGIS (Desktop, Geodatabase, Server, Extensions, Add-ins for Desktop)
+3. Open Source GIS (Q-GIS, Geo-Server, PostgreSQL, CartoView)
+4. Remote Sensing and Digital Image Processing
+5. ERDAS Imagine
+6. Create a Desktop App. using SDK for WPF
+7. Client-Side Technologies (HTML4, CSS2, JavaScript)
+8. Create Web App. Using ArcGIS APIs for JavaScript
+9. Communication, presentation & interviewing skills`,
+      },
+      {
+        school: "Alexandria University",
+        degree: "BA in Geographic Information System, & Geography",
+        graduated: "May 2007",
+        description: `List of subjects:
+The foundations of physical geography (6 months)
+The foundations of human geography (6 months)
+Geography of Africa (3 months)
+Geography of Arab World (3 months)
+Geography of Asia (3 months)
+Introduction to Cartography (4 months)
+Introduction to GIS (3 months)
+Advanced GIS (3 months)
+Aerial photography‎ (3 months)
+Map projection (3 months)
+Geomorphology (6 months)
+Climatology and Meteorology (6 months)
+Hydrology (3 months)
+Biogeography (3 months)
+Oceanography and Limnology (3 months)
+Economic geography (6 months)
+Political Geography (3 months)
+Population geography (6 months)
+Settlement geography (3 months)
+Environmental problems (3 months)
+Geography of Egypt (6 months)`,
+      },
+      {
+        school: "Arab Academy for Science, Technology and Maritime Transport",
+        degree: "Trainee as GIS Specialist",
+        graduated: "Sep 2017",
+        description: `GIS Courses – Grade: A
+1. Fundamentals of GIS
+2. Advanced Editing Auto-CAD Map
+3. Spatial Analysis in ArcGIS
+4. Geo-Database for ArcGIS
+5. Map-Info Professional Basics`,
+      },
+      {
+        school: "Institute of Graduate Stadies and Research (IGSR)",
+        degree: "Trainee as GIS, RS Specialist",
+        graduated: "Dec 2016",
+        description: `GIS Courses – Grade: A
+1. Introduction to ArcGIS Desktop
+2. Working 3D data using ArcGIS
+3. Suitability Analysis Using ArcGIS
+
+Remote Sensing Courses – Grade: A
+1. Image Processing Using ERDAS`,
+      },
+    ],
+
+    //   TODO
+    skillmessage:
+      "Here you can create a short write-up of your skills to show off to employers",
+    skills: [
+      {
+        name: "HTML/5",
+        level: "85%",
+      },
+      {
+        name: "Markdown",
+        level: "75%",
+      },
+      {
+        name: "CSS/3",
+        level: "90%",
+      },
+      {
+        name: "Bootstrap/4",
+        level: "85%",
+      },
+      {
+        name: "JavaScript - ES/10",
+        level: "90%",
+      },
+      {
+        name: "jQuery",
+        level: "90%",
+      },
+      {
+        name: "TypeScript",
+        level: "85%",
+      },
+      {
+        name: "Node.js",
+        level: "95%",
+      },
+      {
+        name: "Express.js",
+        level: "90%",
+      },
+      {
+        name: "Nest.js",
+        level: "95%",
+      },
+      {
+        name: "React",
+        level: "95%",
+      },
+      {
+        name: "Angular/8",
+        level: "85%",
+      },
+      {
+        name: "MongoDB",
+        level: "80%",
+      },
+      {
+        name: "SQL",
+        level: "50%",
+      },
+      {
+        name: "WebPack",
+        level: "85%",
+      },
+      {
+        name: "Firebase",
+        level: "75%",
+      },
+      {
+        name: "Redis",
+        level: "90%",
+      },
+      {
+        name: "RabbitMQ",
+        level: "95%",
+      },
+      {
+        name: "Docker",
+        level: "75%",
+      },
+      {
+        name: "Kubernates",
+        level: "50%",
+      },
+      {
+        name: "REST",
+        level: "95%",
+      },
+      {
+        name: "GraphQL",
+        level: "85%",
+      },
+      {
+        name: "Class-Validator",
+        level: "90%",
+      },
+      {
+        name: "Joi",
+        level: "95%",
+      },
+      {
+        name: "ElasticSearch",
+        level: "85%",
+      },
+      {
+        name: "Git",
+        level: "95%",
+      },
+      {
+        name: "CircleCI 2",
+        level: "80%",
+      },
+      {
+        name: "Debuging",
+        level: "95%",
+      },
+      {
+        name: "Design Patterns",
+        level: "65%",
+      },
+      {
+        name: "PostMan/FireCamp",
+        level: "90%",
+      },
+      {
+        name: "ESRI/Google maps API",
+        level: "95%",
+      },
+      {
+        name: "Geographic Information system",
+        level: "95%",
+      },
+      {
+        name: "ArcGIS (Desktop, Pro)",
+        level: "95%",
+      },
+      {
+        name: "qGIS",
+        level: "95%",
+      },
+      {
+        name: "Remote Sensing",
+        level: "90%",
+      },
+    ],
+  },
+  //   TODO
+  portfolio: {
+    projects: [
+      {
+        title: "Canadian Wanderlust",
+        category: "My Travel Blog for my post-university travels",
+        image: "canadian-wanderlust.jpg",
+        url: "https://www.canadianwanderlust.com",
+      },
+      {
+        title: "Fury Fighting Gear",
+        category: "(offline now) A fighting gear company I started",
+        image: "fury-fighting-gear.jpg",
+        url: "http://www.timbakerdev.com",
+      },
+      {
+        title: "Original Thai Food",
+        category: "Website I built for a restaurant I like in Thailand",
+        image: "original-thai-food.jpg",
+        url: "http://www.timbakerdev.com/originalthaifood.github.io",
+      },
+      {
+        title: "Resume Website",
+        category: "A React based resume website template",
+        image: "resume-website.jpg",
+        url: "http://www.timbakerdev.com",
+      },
+      {
+        title: "Smirkspace",
+        category:
+          "(MVP Only) A React and Meteor based chat University project.",
+        image: "smirkspace.jpg",
+        url: "http://www.smirkspace.com",
+      },
+    ],
+  },
+  //   TODO
+  testimonials: {
+    testimonials: [
+      {
+        text:
+          "Here you should write some nice things that someone has said about you. No lies though, employers can tell when you are lying.",
+        user: "Kareem Abdul Jabbar",
+      },
+      {
+        text:
+          "That Tim Baker must be one of the most brilliant developers I've ever met! It is amazing that nobody has hired him yet. Hey you, you should hire this guy, he may be fresh out of University and have zero on the job experience but I am confident that he will be one of your best developers in no time!",
+        user: "Steve Wozniak... impersonator",
+      },
+    ],
+  },
+};
