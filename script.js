18fec9c6d9213279ddc70a6b664aca6688da8c4c
Hisham T. Kamal-Al-Din <hisham.taha.work@gmail.com>
2020-12-31 22:51:17 +0000

    edits

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+
 const filesPrefix = '/hisham.work/';
 const filePath = path.join(process.cwd(), 'docs', 'asset-manifest.json');
-const fileData = JSON.parse(fs.readFileSync(filePath).toString());
-fileData.entrypoints = fileData.entrypoints.map((str) => filesPrefix + str);
-fs.writeFileSync(filePath, JSON.stringify(fileData, '', '\t'));
+
+async function init() {
+  const fileData = JSON.parse(await fs.readFileSync(filePath).toString());
+  fileData.entrypoints = fileData.entrypoints.map((str) => filesPrefix + str);
+  await fs.writeFileSync(filePath, JSON.stringify(fileData, '', '\t'));
+}
+
+init();
